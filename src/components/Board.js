2c34f28a1ca1e38c78c8467d9410ba09d4482c0c
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-09 21:33:17 +0200

    adding node options to json

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,9 +3,7 @@ import { useSelector } from 'react-redux'
 import { Square } from 'components/Square'
 
 export const Board = () => {
-  
   const squares = useSelector((store) => store.game.squares)
-
   return (
     <div className="board">
       {squares.map((value, index) => (
